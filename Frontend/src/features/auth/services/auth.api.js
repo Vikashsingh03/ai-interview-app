c73123826aa701b62b16425abd97860d1129e84c
Vikash Kumar Singh <vikashsinghbxr8538@gmail.com>
2026-03-22 21:35:19 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://ai-interview-app1.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
